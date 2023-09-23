94595c0642c518fe68d0cb4f02b60b9590520d13
ranjeetksingh55 <ranjeetksingh693@gmail.com>
2023-09-24 00:06:33 +0530

    Operations and converstions

--- basics/conversionOpertation.js
+++ basics/conversionOpertation.js
@@ -36,8 +36,63 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ********************************** Operations *******************************************
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+console.log(2+3);
+console.log(16/3);
+console.log(3*4);
+console.log(44-33);
+console.log(33**2); // power
+console.log(2%4);
+
+let str1 = "hello"
+let str2 = " ranjeet"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2 + 4);
+// console.log("1" + 2);
+// console.log(3 + "1");
+// console.log(1 + 2 + "3");
+
+// console.log((3+4) * 5 % 3); // user proper pranthesis 
+
+// console.log(+true);
+// console.log(true);
+// console.log(+"");
+
+
+let num1, num2, num3
+num1= num2 = num3 = 2 + 4
+console.log(num1);
+
+// let gameCounter = 100
+// gameCounter++;              // Increment (++) //Postfix increment
+// console.log(gameCounter);
+
+// what will we do in prefix and postfix
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter); //Prefix increment
+
+
+
+
+
+
+
+
+
+
 
 
 
