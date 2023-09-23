52295d9a836f2691a0bb011ec78904c17fe38aea
ranjeetksingh55 <ranjeetksingh693@gmail.com>
2023-09-23 23:21:17 +0530

    Conversion of datatypes

---
+++ basics/conversionOpertation.js
@@ -0,0 +1,44 @@
+//let score = "343abc"  //nan(not a number)
+
+// let score = null
+
+// let score  undefined
+
+// let score = true;
+
+// let score = false
+
+let score = "ranjeet"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber) //nan (not a number)
+
+
+// "33" => "33"
+// "33rxc" => NaN
+// true = 1
+// false = 0
+
+let isLoggedIn = "1"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1 => True
+// 0 => false
+// "" => false
+// "ranjeet" = true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
