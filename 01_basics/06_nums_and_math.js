30518dead8d9600b55f4b7f4fb30f01d68d1c85c
ranjeetksingh55 <ranjeetksingh693@gmail.com>
2023-09-27 00:09:13 +0530

    math and num

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,3 +1,43 @@
+// Numbers
+
+
 const score = 400
-const balance = new Number(100)
-console.log(balance);
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8934
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//Maths
+
+// console.log(Math);
+// console.log(Math.abs(-4));    // absolute value -ve will be changes to +ve (-4 to 4)
+// console.log(Math.round(4.6));   // round off
+// console.log(Math.ceil(4.5));   // ceiling means if number is greater than 4 than it will choose 5
+// console.log(Math.floor(4.9));   // it will choose 4 because it will choose low value. (4)
+// console.log(Math.min(4,5,3,1));  // it will choose lower value (1)
+
+
+console.log(Math.random());  // random value always come between 0 to 1.
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+// Formula to find max and min
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max - min + 1))+ min)
+
+
+
+
+
