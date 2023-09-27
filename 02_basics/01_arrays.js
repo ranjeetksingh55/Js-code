280316522c14722ec95d1072b090252ccbdbc683
ranjeetksingh55 <ranjeetksingh693@gmail.com>
2023-09-28 00:33:31 +0530

    Array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,18 @@
+// array
+
+const myArr = [0,1,2,3,4,5]
+const myHeros = ["Hanuman", "Krish","thor"]
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr[1]);
+
+// methods of array
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.push(8)
+// myArr.pop()   // pop remove last value of array
+
+// myArr.unshift(9)
+myArr.shift()   // [ 1, 2, 3, 4, 5 ]
+
+console.log(myArr); 
