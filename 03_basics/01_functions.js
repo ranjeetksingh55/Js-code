8e2c193e863da0507e06c909ec7b6dd5edcb303b
ranjeetksingh55 <ranjeetksingh693@gmail.com>
2023-10-18 23:13:03 +0530

    function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -34,5 +34,34 @@ function loginUserMessage(username = "sam"){
 console.log(loginUserMessage("hitesh"))
 // console.log(loginUserMessage("hitesh"))
 
+function calculateCartPrice(...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200, 300, 400))
+
+const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
+
+
 
 
