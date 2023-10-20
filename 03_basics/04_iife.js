d9a65f15a5a10bc655dc1006b614a1a3b6b782bb
ranjeetksingh55 <ranjeetksingh693@gmail.com>
2023-10-20 12:15:05 +0530

    iife in js

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,22 @@
+// immediately invoked functions expression (iife)
+
+(function chai (){
+    console.log(`DB CONNECTED`);
+
+})();  // if we did't close the semicolon then it will give error.  
+// iife syntax curly brases close with parenthesis 
+// chai()
+
+// Avoid polluting the global namespace - iife
+
+( (name)=>{
+    console.log(`DB CONNECTED Two ${name}`);
+})('ranjeet')
+
+// write two iife (interview question)
+
+
+
+
+
+
