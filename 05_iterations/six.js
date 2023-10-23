9a407c73ec6c1af4bc3b82f1d81e90166eb628d2
ranjeetksingh55 <ranjeetksingh693@gmail.com>
2023-10-23 23:54:33 +0530

    filter

---
+++ 05_iterations/six.js
@@ -0,0 +1,20 @@
+// const coding = ["js", "rb", "py", "java", "cpp"]
+
+// const values = coding.forEach ((item)=> {
+//     console.log(item);
+// })
+
+// console.log(values);
+
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNums.filter ( (num)=> num > 5)
+
+// console.log(newNums);
+
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const newNums = myNums.filter ()
+
+console.log(newNums);
